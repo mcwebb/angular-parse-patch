1c495fdde760e598fd932a1d1718cfd2db093477
Matthew Webb <matthewowebb@gmail.com>
2015-01-29 23:50:53 +0800

    added ngAnotate to build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
 
 // Lint Task
 gulp.task('lint', function() {
-    gulp.src('./src/parse-angular.js')
+    gulp.src('./src/angular-parse-patch.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('uglify', function() {
-    gulp.src('./src/parse-angular.js')
-        .pipe(uglify())
+    gulp.src('./src/angular-parse-patch.js')
+    	.pipe(ngAnnotate())
+        .pipe(uglify({ mangle: false }))
         .pipe(gulp.dest('./dist'));
 });
 
