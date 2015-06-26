f32f16c70c5399861d257011aae20ccd5d2ccba9
Matthew Webb <matthewowebb@gmail.com>
2015-06-26 17:43:00 +0800

    major update, no longer auto-wraps Parse promises

--- src/angular-parse-patch.js
+++ src/angular-parse-patch.js
@@ -1,164 +1,12 @@
-/*
- * Part of mcwebb/angular-parse-patch
- * Coypyright 2015 Matthew Webb <matthewowebb@gmail.com>
- * MIT License
- * Based on @brandid/parse-angular-patch released under the MIT license (2014)
- */
-(function (window) {
-	var angular = window.angular,
-		module = angular.module('mcwebb.parse-patch', []),
-		methodsToUpdate = {
-			'Object': {
-				instance: ['destroy', 'fetch', 'save'],
-				// 'fetchAll' and 'saveAll' not advertised as returning promise but currently does
-				class: ['destroyAll', 'fetchAll', 'saveAll']
-			},
-			'Collection': {
-				instance: ['fetch'],
-				class: []
-			},
-			'Query': {
-				// 'get' not advertised as returning promise but currently does
-				instance: ['count', 'each'],
-				class: []
-			},
-			'Cloud': {
-				instance: [],
-				class: ['httpRequest', 'run']
-			},
-			'User': {
-				instance: ['logIn', 'signUp'],
-				// 'requestPasswordReset' not advertised as returning promise but currently does
-				class: ['become', 'logIn', 'requestPasswordReset', 'signUp']
-			},
-			'FacebookUtils': {
-				instance: [],
-				class: ['logIn', 'link', 'unlink']
-			},
-			'File': {
-				instance: ['save'],
-				class: []
-			},
-			'Promise': {
-				instance: [],
-				class: ['send']
-			}
-		};
+(function (angular) {
+	var internal = {};
 
-	module.provider('ngParse', function () {
-		var credentials = {
-			appId: '',
-			jsKey: ''
-		};
+	internal.credentials = {
+		appId: '',
+		jsKey: ''
+	};
 
-		this.initialize = function (appId, jsKey) {
-			credentials.appId = appId;
-			credentials.jsKey = jsKey;
-		};
-
-		this.$get = function ($window, $rootScope, $q) {
-			// fail if the vendor Parse sdk is not available
-			if (angular.isUndefined($window.Parse))
-				throw new Error('Parse SDK not available');
-
-			// get a copy to retrofit
-			var ngParse = $window.Parse;
-
-			ngParse.initialize(
-				credentials.appId,
-				credentials.jsKey
-			);
-
-			// loop over ngParse objects
-			for (var k in methodsToUpdate) {
-				var currentClass = k;
-				var currentObject = methodsToUpdate[k];
-
-				/* TODO: figure out a way to return an Angular and Parse
-				 * compatible object so that when Parse makes internal calls
-				 * we won't get errors e.g. _thenRunCallbacks()
-				 */
-				/// Patching instance methods
-				currentObject.instance.forEach(function (method) {
-					var origMethod = ngParse[currentClass].prototype[method];
-					// run the original function but return an angular promise
-					ngParse[currentClass].prototype[method] = function () {
-						$rootScope.$broadcast('parse:transfer:start');
-						var parsePromise = origMethod.apply(this, arguments),
-							defer = $q.defer();
-						parsePromise.then(function (data) {
-							defer.resolve(data);
-						}, function (err) {
-							defer.reject(err);
-						});
-                        if (typeof parsePromise.finally === 'function') {
-                            parsePromise.finally(function () {
-                                $rootScope.$broadcast('parse:transfer:end');
-                            });
-                        }
-                        else if (typeof parsePromise.always === 'function') {
-                            parsePromise.always(function () {
-                                $rootScope.$broadcast('parse:transfer:end');
-                            });
-                        }
-						return defer.promise;
-					};
-				});
-
-				// patching class methods too
-				currentObject.class.forEach(function (method) {
-					var origMethod = ngParse[currentClass][method];
-					// run the original function but return an angular promise
-					ngParse[currentClass][method] = function () {
-						$rootScope.$broadcast('parse:transfer:start');
-						var parsePromise = origMethod.apply(this, arguments),
-							defer = $q.defer();
-						parsePromise.then(function (data) {
-							defer.resolve(data);
-						}, function (err) {
-							defer.reject(err);
-						});
-                        if (typeof parsePromise.finally === 'function') {
-                            parsePromise.finally(function () {
-                                $rootScope.$broadcast('parse:transfer:end');
-                            });
-                        }
-                        else if (typeof parsePromise.always === 'function') {
-                            parsePromise.always(function () {
-                                $rootScope.$broadcast('parse:transfer:end');
-                            });
-                        }
-						return defer.promise;
-					};
-				});
-			}
-
-			/*
-			 * Including the below would allow custom initialization on User
-			 * objects without calling the parent. But the same can't be
-			 * accomplished for the Parse.Object, so for the sake of consistency
-			 * I have left it out.
-				// no need to overwrite anything, Parse knows.
-				ngParse.User.extend({
-					constructor: function () {
-						if (typeof this.fields === 'object')
-							enablePropAccess(this, this.fields);
-						ngParse.Object.apply(this, arguments);
-					}
-				});
-			*/
-
-			// this runs for both Object and User since User inherits.
-			ngParse.Object.prototype.initialize = function () {
-				if (typeof this.fields === 'object')
-					enablePropAccess(this, this.fields);
-			};
-
-			return ngParse;
-		};
-	});
-
-	function generateProps(instance, fields) {
+	internal.generateProps = function (instance, fields) {
 		var props = {};
 		angular.forEach(fields, function (name) {
 			props[name] = {
@@ -171,13 +19,13 @@
 			};
 		});
 		return props;
-	}
+	};
 
-	function enablePropAccess(instance, fields) {
+	internal.enablePropAccess = function (instance, fields) {
 		if (typeof Object.defineProperties != 'undefined') {
 			Object.defineProperties(
 				instance,
-				generateProps(instance, fields)
+				internal.generateProps(instance, fields)
 			);
 		} else {
 			angular.forEach(fields, function (name) {
@@ -189,5 +37,54 @@
 				});
 			});
 		}
+	};
+	
+	function ngParseProvider () {
+		this.initialize = function (appId, jsKey) {
+			internal.credentials.appId = appId;
+			internal.credentials.jsKey = jsKey;
+		};
+
+		this.$get = function ($window, $q) {
+			// fail if the vendor Parse sdk is not available
+			if (angular.isUndefined($window.Parse))
+				throw new Error('Parse SDK not available');
+
+			// get a reference
+			var ngParse = $window.Parse;
+
+			ngParse.initialize(
+				internal.credentials.appId,
+				internal.credentials.jsKey
+			);
+			
+			// this runs for both Object and User since User inherits.
+			ngParse.Object.prototype.initialize = function () {
+				if (typeof this.fields === 'object')
+					internal.enablePropAccess(this, this.fields);
+			};
+			
+			// angularify promises if you want 
+			ngParse.$q = function (parsePromise) {
+				if (!ngParse.Promise.is(parsePromise))
+					throw new Error('ngParse.$q only accepts Parse.Promise');
+					
+				var deferred = $q.defer();
+				// Parse.Promise doesn't implement a notify method 
+				parsePromise.then(function (result) {
+					deferred.resolve(result);
+				}, function (error) {
+					deferred.reject(error);
+				});
+				
+				return deferred.promise;
+			};
+
+			return ngParse;
+		};
 	}
-}) (this);
+
+	angular
+		.module('mcwebb.parse-patch', [])
+		.provider('ngParse', ngParseProvider);
+})(window.angular);
